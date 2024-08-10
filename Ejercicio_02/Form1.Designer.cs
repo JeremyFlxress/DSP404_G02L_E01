2c357f7699398ef9d9aa8c1a746f40aa42aeb90b
Josué Eduardo García Estrada <ge240098@alumno.udb.edu.sv>
2024-08-10 10:42:24 -0600

    Se agrego un campo de ingresar nombre y el panel del cajero

--- Ejercicio_02/Form1.Designer.cs
+++ Ejercicio_02/Form1.Designer.cs
@@ -36,16 +36,21 @@
             this.btnIngresar = new System.Windows.Forms.Button();
             this.pnlInicio = new System.Windows.Forms.Panel();
             this.pnlCajero = new System.Windows.Forms.Panel();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombreUsuario = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.pnlInicio.SuspendLayout();
+            this.pnlCajero.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(292, 25);
+            this.pictureBox1.Location = new System.Drawing.Point(309, 21);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(209, 187);
+            this.pictureBox1.Size = new System.Drawing.Size(188, 142);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -55,7 +60,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(232, 234);
+            this.label1.Location = new System.Drawing.Point(236, 185);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(322, 37);
             this.label1.TabIndex = 1;
@@ -66,7 +71,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(194, 309);
+            this.label2.Location = new System.Drawing.Point(199, 312);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(225, 21);
             this.label2.TabIndex = 2;
@@ -74,7 +79,7 @@
             // 
             // txtCuenta
             // 
-            this.txtCuenta.Location = new System.Drawing.Point(425, 306);
+            this.txtCuenta.Location = new System.Drawing.Point(430, 309);
             this.txtCuenta.Name = "txtCuenta";
             this.txtCuenta.Size = new System.Drawing.Size(197, 29);
             this.txtCuenta.TabIndex = 3;
@@ -83,7 +88,7 @@
             // 
             this.btnIngresar.BackColor = System.Drawing.Color.Gold;
             this.btnIngresar.ForeColor = System.Drawing.Color.Black;
-            this.btnIngresar.Location = new System.Drawing.Point(365, 377);
+            this.btnIngresar.Location = new System.Drawing.Point(356, 386);
             this.btnIngresar.Name = "btnIngresar";
             this.btnIngresar.Size = new System.Drawing.Size(115, 39);
             this.btnIngresar.TabIndex = 4;
@@ -94,13 +99,15 @@
             // pnlInicio
             // 
             this.pnlInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pnlInicio.Controls.Add(this.txtNombreUsuario);
+            this.pnlInicio.Controls.Add(this.label4);
             this.pnlInicio.Controls.Add(this.pictureBox1);
             this.pnlInicio.Controls.Add(this.btnIngresar);
             this.pnlInicio.Controls.Add(this.label1);
             this.pnlInicio.Controls.Add(this.txtCuenta);
             this.pnlInicio.Controls.Add(this.label2);
-            this.pnlInicio.Controls.Add(this.pnlCajero);
             this.pnlInicio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlInicio.ForeColor = System.Drawing.Color.White;
             this.pnlInicio.Location = new System.Drawing.Point(0, 0);
             this.pnlInicio.Name = "pnlInicio";
             this.pnlInicio.Size = new System.Drawing.Size(816, 464);
@@ -108,12 +115,54 @@
             // 
             // pnlCajero
             // 
+            this.pnlCajero.BackColor = System.Drawing.Color.Blue;
+            this.pnlCajero.Controls.Add(this.label3);
+            this.pnlCajero.Controls.Add(this.btnVolver);
             this.pnlCajero.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlCajero.Location = new System.Drawing.Point(0, 0);
             this.pnlCajero.Name = "pnlCajero";
             this.pnlCajero.Size = new System.Drawing.Size(816, 464);
             this.pnlCajero.TabIndex = 5;
             this.pnlCajero.Visible = false;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.Gold;
+            this.btnVolver.Location = new System.Drawing.Point(365, 331);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(94, 40);
+            this.btnVolver.TabIndex = 0;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(325, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 32);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "¡Bienvenio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(199, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Digite su nombre:";
+            // 
+            // txtNombreUsuario
+            // 
+            this.txtNombreUsuario.Location = new System.Drawing.Point(430, 265);
+            this.txtNombreUsuario.Name = "txtNombreUsuario";
+            this.txtNombreUsuario.Size = new System.Drawing.Size(197, 29);
+            this.txtNombreUsuario.TabIndex = 6;
             // 
             // Form1
             // 
@@ -122,14 +171,18 @@
             this.BackColor = System.Drawing.Color.Blue;
             this.ClientSize = new System.Drawing.Size(816, 464);
             this.Controls.Add(this.pnlInicio);
+            this.Controls.Add(this.pnlCajero);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Cajero electrónico";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.pnlInicio.ResumeLayout(false);
             this.pnlInicio.PerformLayout();
+            this.pnlCajero.ResumeLayout(false);
+            this.pnlCajero.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -143,6 +196,10 @@
         private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Panel pnlInicio;
         private System.Windows.Forms.Panel pnlCajero;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombreUsuario;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Ejercicio_02/Form1.cs
+++ Ejercicio_02/Form1.cs
@@ -17,9 +17,18 @@ namespace Ejercicio_02
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Cuando se ejecute el programa siempre aparezca el panel de Incio
+            pnlInicio.Visible = true;
+            pnlCajero.Visible = false;
+            txtNombreUsuario.Select();
+        }
+
         //Declaración de arreglos
         public static int[] numero_cuenta = { 10001, 10002, 10003, 10004, 10005 };
         public static int[] saldo_cuenta = { 400, 800, 1200, 1500, 2000 };
+        public static string nombre_usuario;
 
         //Metodo para ver si es un número o no
         public static Boolean IsNumeric(string valor)
@@ -31,42 +40,68 @@ namespace Ejercicio_02
         private void btnIngresar_Click(object sender, EventArgs e)
         {
             string valor_usuario = txtCuenta.Text;
+            nombre_usuario = txtNombreUsuario.Text;
+            bool nombre_valido = nombre_usuario.Length >= 3;
 
-            if(!IsNumeric(valor_usuario) || valor_usuario == null)
+            if (!nombre_valido)
+            {
+                MessageBox.Show("Digite un nombre correcto", "Advertencia",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNombreUsuario.Clear();
+                txtNombreUsuario.Focus();
+                return;
+            }
+            else
+            {
+                txtCuenta.Focus();
+            }
+
+            if (!IsNumeric(valor_usuario) || valor_usuario == null)
             {
                 MessageBox.Show("Digite un número de cuenta correcto", "Error",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
                 txtCuenta.Clear();
                 txtCuenta.Focus();
+                return;
             }
-            else 
+
+            int valor_cuenta = Convert.ToInt32(txtCuenta.Text);
+            bool cuentaValida = false;
+
+            for (int i = 0; i < numero_cuenta.Length; i++)
             {
-                int valor_cuenta = Convert.ToInt32(txtCuenta.Text);
-                bool cuentaValida = false;
-
-                for (int i = 0; i < numero_cuenta.Length; i++)
+                // Validación de si el usuario digitó el número de cuenta correcto
+                if (numero_cuenta[i] == valor_cuenta && nombre_valido)
                 {
-                    //Validación de si el usuario digito el numero de cuenta correcto
-                    if (numero_cuenta[i] == valor_cuenta)
-                    {
-                        cuentaValida = true;
-                        MessageBox.Show("Número de cuenta de cuenta correcto", "Ingreso Exitosamente",
-                            MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        pnlInicio.Visible = false;
-                        pnlCajero.Visible = true;
-                        break;
-
-                    }
+                    cuentaValida = true;
+                    MessageBox.Show("Número de cuenta correcto", "Ingreso Exitosamente",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    pnlInicio.Visible = false;
+                    pnlCajero.Visible = true;
+                    break;
                 }
+            }
 
-                if (!cuentaValida)
-                {
-                    MessageBox.Show("No existe ese número de cuenta. Digite uno correcto", "Información",
-                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                    txtCuenta.Clear();
-                    txtCuenta.Focus();
-                }
+            if (!cuentaValida)
+            {
+                MessageBox.Show("No existe ese número de cuenta. Digite uno correcto", "Información",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtCuenta.Clear();
+                txtCuenta.Focus();
             }
         }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            pnlCajero.Visible = false;
+            pnlInicio.Visible = true;
+            //Limpiar los textboxs
+            txtNombreUsuario.Clear();
+            txtCuenta.Clear();
+
+            txtNombreUsuario.Focus();
+        }
+
+
     }
 }
