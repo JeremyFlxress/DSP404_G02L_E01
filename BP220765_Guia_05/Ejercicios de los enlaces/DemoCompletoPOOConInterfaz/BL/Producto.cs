7d2d85f36c8d6e3b5b5953d5d9d887499e40a246
unknown <LiamXG04>
2024-09-03 20:47:25 -0600

    subiendo guia 5

---
+++ BP220765_Guia_05/Ejercicios de los enlaces/DemoCompletoPOOConInterfaz/BL/Producto.cs
@@ -0,0 +1,13 @@
+﻿namespace BL
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public float Cantidad  { get; set; }
+        public decimal Valor { get { return Precio * (decimal)Cantidad; } }
+        
+    }
+}
+    
