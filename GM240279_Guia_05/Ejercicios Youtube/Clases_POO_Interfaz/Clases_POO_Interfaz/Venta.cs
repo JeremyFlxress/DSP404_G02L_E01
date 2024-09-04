b7a83e6e3f4961381ac874f9327121d914e06b75
Oche <GM240279@alumno.udb.edu.sv>
2024-09-03 18:57:44 -0600

    Finalizacion ejercicio guia

---
+++ GM240279_Guia_05/Ejercicios Youtube/Clases_POO_Interfaz/Clases_POO_Interfaz/Venta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_POO_Interfaz
+{
+    internal class Venta
+    {
+        private string cliente;
+        private DateTime fecha;
+        private List<Productos> productos;
+
+        public Venta(string cliente) { 
+            this.cliente = cliente;
+            this.fecha = DateTime.Now;
+            this.productos = new List<Productos>();
+        }
+
+        public void AgregarProducto(Productos producto)
+        {
+            productos.Add(producto);
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0;
+            foreach (Productos producto in productos) {
+                total += producto.Valor;
+            }
+            return total;
+        }
+    }
+}
