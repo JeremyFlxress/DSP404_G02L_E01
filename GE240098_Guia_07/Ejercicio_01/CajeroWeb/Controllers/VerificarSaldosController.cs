ec8484fde2d89b0a8c144391d2dc665db8f77684
Josué Eduardo García Estrada <ge240098@alumno.udb.edu.sv>
2024-09-13 16:57:26 -0600

    Guia 7 terminada

---
+++ GE240098_Guia_07/Ejercicio_01/CajeroWeb/Controllers/VerificarSaldosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CajeroWeb.Controllers
+{
+    public class VerificarSaldosController : Controller
+    {
+        // GET: VerificarSaldos
+        public ActionResult VerificarSaldos()
+        {
+            return View();
+        }
+    }
+}
