1073f77f4eaeadb94faee785e26af6025336c0e5
Josué Eduardo García Estrada <ge240098@alumno.udb.edu.sv>
2024-09-13 12:06:50 -0600

    Cambios realizados hasta el punto 32

---
+++ GE240098_Guia_07/Ejercicio_01/CajeroWeb/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CajeroWeb.Models;
+
+namespace CajeroWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string sNumeroTarjeta, string sNumeroPIN)
+        {
+            double sSaldo;
+            string numero;
+            Transaccion nuevaTransaccion = new Transaccion(sNumeroTarjeta);
+            if (sNumeroTarjeta == "123456" && sNumeroPIN == "654321")
+            {
+                numero = nuevaTransaccion.getNumeroTarjeta();
+                sSaldo = nuevaTransaccion.getSaldo();
+                return Redirect("~/Menu/Menu?sNumeroTarjeta=" + sNumeroTarjeta + "&sSaldo=" + sSaldo);
+            }
+            else
+            {
+                //Si el numero de pin o de tarjeta no corresponde, entonces se
+                // procede a mostrar a un mensaje de error
+                ViewBag.Error = "Error en numero de tarjeta o PIN. Verifique";
+                return View();
+            }
+        }
+    }
+}
---
+++ GE240098_Guia_07/Ejercicio_01/CajeroWeb/Models/Transaccion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace CajeroWeb.Models
+{
+    public class Transaccion
+    {
+        private string sNumeroTarjeta;
+        private double fSaldo;
+        //Constructor de la clase que solo acepta un parametro de tipo string
+        public Transaccion(string tarjeta)
+        {
+            sNumeroTarjeta = tarjeta;
+            fSaldo = 500;
+        }
+        /*
+        Constructor de clase que acepta dos parametros: String y double. 
+        Puede haber tantos constructores como se necesiten, pero c/definición debe tener
+        una firma (lista y orden de tipos de datos en parametros) diferente
+         */
+        public Transaccion(string tarjeta, double saldo)
+        {
+            sNumeroTarjeta = tarjeta;
+            fSaldo = saldo;
+        }
+        //Implementación de métodos get y set para acceder a campos de clase
+        public double getSaldo()
+        {
+            return fSaldo;
+        }
+        public void setSaldo(double saldo)
+        {
+            fSaldo = saldo;
+        }
+        public string getNumeroTarjeta()
+        {
+            return sNumeroTarjeta;
+        }
+        public void setNumeroTarjeta(string tarjeta)
+        {
+            sNumeroTarjeta = tarjeta;
+        }
+        public void transferenciaNacional(double monto)
+        {
+            fSaldo = fSaldo - monto;
+        }
+        public void transferenciaInternacional(double monto)
+        {
+            fSaldo = fSaldo - monto;
+        }
+    }
+}
