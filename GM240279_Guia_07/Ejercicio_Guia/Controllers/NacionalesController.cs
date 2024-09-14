c4c9872fbee19559275716b586032133185b6e6c
Oche <GM240279@alumno.udb.edu.sv>
2024-09-14 11:43:15 -0600

    Finalizacion Guia

---
+++ GM240279_Guia_07/Ejercicio_Guia/Controllers/NacionalesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CajeroWeb.Models;
+
+namespace CajeroWeb.Controllers
+{
+    public class NacionalesController : Controller
+    {
+        // GET: Nacionales
+        public ActionResult Nacionales(string sNumeroTarjeta, double? sSaldo)
+        {
+            // Verificamos si sNumeroTarjeta está nulo o vacío
+            if (string.IsNullOrEmpty(sNumeroTarjeta) || sSaldo == null)
+            {
+                // Devuelve un mensaje de error o redirige al menú inicial
+                return Redirect("~/Home/Error");
+            }
+
+            ViewBag.sNumeroTarjeta = sNumeroTarjeta;
+            ViewBag.sSaldo = sSaldo.Value;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nacionales(string sNumeroTarjeta, double? sSaldo, double cantidad)
+        {
+            // Validamos si el saldo o el número de tarjeta son nulos
+            if (string.IsNullOrEmpty(sNumeroTarjeta) || sSaldo == null)
+            {
+                return Redirect("~/Home/Error");
+            }
+
+            Transaccion nuevaTransaccion = new Transaccion(sNumeroTarjeta, sSaldo.Value);
+
+            if (cantidad > 0)
+            {
+                nuevaTransaccion.transferenciaNacional(cantidad);
+            }
+
+            sSaldo = nuevaTransaccion.getSaldo();
+            ViewBag.sNumeroTarjeta = nuevaTransaccion.getNumeroTarjeta();
+            ViewBag.sSaldo = nuevaTransaccion.getSaldo();
+
+            return Redirect("~/Menu/Menu?sNumeroTarjeta=" + sNumeroTarjeta + "&sSaldo=" + sSaldo);
+        }
+
+    }
+}
--- GM240279_Guia_07/Ejercicio_Guia/Ejercicio_Guia/Ejercicio_Guia/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejercicio_Guia.MvcApplication" Language="C#" %>
