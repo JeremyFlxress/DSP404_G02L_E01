c62f3fe609b072d7d161c41533eef27139c2e3e4
Josué Eduardo García Estrada <ge240098@alumno.udb.edu.sv>
2024-09-14 19:03:17 -0600

    Primeras clases terminadas.

---
+++ GE240098_Guia_08/SitioWebGE240098/Models/categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitioWebGE240098.Models
+{
+    public class categoria
+    {
+        public List<SelectListItem> Categorias { get; set; }
+        public int nIdCategoria;
+        public string sNombreCategoria;
+    }
+}
