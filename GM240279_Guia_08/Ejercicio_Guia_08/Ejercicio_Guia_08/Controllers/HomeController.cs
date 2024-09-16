039bce6cdba495d59368ae8fe10dcd7e691dc815
Oche <GM240279@alumno.udb.edu.sv>
2024-09-15 19:56:17 -0600

    Guia 8

---
+++ GM240279_Guia_08/Ejercicio_Guia_08/Ejercicio_Guia_08/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Ejercicio_Guia_08.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio_Guia_08.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Conexion obj = new Conexion();
+            ViewBag.OpcionesCat = obj.getCategorias();
+            ViewBag.Ofertas = obj.CargarDatos();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string OpcionesCat) {  }
+    }
+
+}
---
+++ GM240279_Guia_08/Ejercicio_Guia_08/Ejercicio_Guia_08/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejercicio_Guia_08.MvcApplication" Language="C#" %>
