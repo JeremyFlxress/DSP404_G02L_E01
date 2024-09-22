fdb63f170a1d4a1597aeb0f65ac0394feac9ab98
Oche <GM240279@alumno.udb.edu.sv>
2024-09-22 15:58:20 -0600

    Parte II Login: Cambios modelos y clases
    Se ha creado la linea de conexion a la base de datos en web.config con entity framework ya en solucion, con su respectivo modelo. Se han creado las clases para las tablas empleados y clientes.

---
+++ Desafio_01/Parte 2/Login/Login/Models/ModelPedidosDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Login.Models
+{
+    public class ModelPedidosDB : DbContext
+    {
+        public ModelPedidosDB() : base("PedidosDBContext")
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
