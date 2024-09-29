5e91ff9633b64f236669abc9db641ddd7064c65b
Josué Eduardo García Estrada <ge240098@alumno.udb.edu.sv>
2024-09-29 17:13:23 -0600

    Clase Categoria Agregada

---
+++ GE240098_Guia_09/SitioWeb9_GE240098/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitioWeb9_GE240098.Models
+{
+    public class Category
+    {
+        public List<SelectListItem> Categorias { get; set; }
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public int Description { get; set; }
+    }
+}
