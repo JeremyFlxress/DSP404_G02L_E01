689c1eaed16be3f71b870e9ef210cb6cd17983af
unknown <LiamXG04>
2024-10-01 22:31:57 -0600

    terminada Guia 09

---
+++ BP220765_Guia_09/SitioWeb_Guia09/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+
+namespace SitioWeb_Guia09.Models
+{
+    public class Product
+    {
+        public List<Product> Productos { get; set; }
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string SupplierID { get; set; }
+        public string CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public string UnitPrice { get; set; }
+        public string UnitsInStock { get; set; }
+        public string UnitsOnOrder { get; set; }
+    }
+}
