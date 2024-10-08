e27a9913bd42ae72bb5feadfc5d73c2a868af7c8
Oche <GM240279@alumno.udb.edu.sv>
2024-10-07 20:01:21 -0600

    Archivos

---
+++ GM240279_Guia_11/Recursos Guia 11/WebService/WebService/AgregarContacto.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebService
+{
+    public partial class AgregarContacto : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["usuario"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            Label2.Text = "AGENDA " + (string)Session["usuario"];
+        }
+
+        protected void BtnGuardar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
