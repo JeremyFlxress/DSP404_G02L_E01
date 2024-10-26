18a40433278fe7d6684c9838c423b41c304b2358
JeremyFlxress <FP240479@alumno.udb.edu.sv>
2024-10-26 12:06:18 -0600

    Creando lógica de empleados a usuario
    Funciona Perfectamente el apartado de crearconcierto y que se lo muestre al usuario

--- Desafio_03/TurnoverTicket/Controllers/ComprarConciertoController.cs
+++ Desafio_03/TurnoverTicket/Controllers/ComprarConciertoController.cs
@@ -3,15 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TurnoverTicket.Models;
 
 namespace TurnoverTicket.Controllers
 {
     public class ComprarConciertoController : Controller
     {
+        private Turnover_SA_CVEntities db = new Turnover_SA_CVEntities();
+
         // GET: ComprarConcierto
         public ActionResult ComprarConcierto()
         {
-            return View();
+            var conciertos = db.Conciertos
+                .Where(c => c.Fecha >= DateTime.Now)  
+                .OrderBy(c => c.Fecha)
+                .Select(c => new
+                {
+                    Concierto = c,
+                    PrecioMinimo = c.Entradas.Min(e => e.Precio)
+                })
+                .ToList()
+                .Select(x => new ComprarConcierto
+                {
+                    IDConcierto = x.Concierto.IDConcierto,
+                    Nombre = x.Concierto.Nombre,
+                    Artista = x.Concierto.Artista,
+                    Fecha = x.Concierto.Fecha,
+                    Localizacion = x.Concierto.Localizacion,
+                    PrecioDesde = x.PrecioMinimo
+                })
+                .ToList();
+
+            return View(conciertos);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ Desafio_03/TurnoverTicket/Models/ComprarConcierto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TurnoverTicket.Models
+{
+    public class ComprarConcierto
+    {
+        
+       
+    public int IDConcierto { get; set; }
+        public string Nombre { get; set; }
+        public string Artista { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Localizacion { get; set; }
+        public decimal PrecioDesde { get; set; }
+    
+
+}
+}
