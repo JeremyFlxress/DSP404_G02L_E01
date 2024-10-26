1e2362debbb4cc37faf9ff932e917bb8cf576a0b
unknown <LiamXG04>
2024-10-26 16:08:13 -0600

    login

---
+++ login/login/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace login.Controllers
+{
+	public class LoginController : Controller
+	{
+
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		// Acción para la página de selección de usuario
+		public ActionResult SeleccionUsuario()
+		{
+			return View();
+		}
+
+		// Acción para el login de cliente
+		public ActionResult Cliente()
+		{
+			return View();
+		}
+
+		// Acción para el registro de cliente
+		public ActionResult RegistroCliente()
+		{
+			return View();
+		}
+	}
+}
