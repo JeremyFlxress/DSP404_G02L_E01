36c945f7a8791933d655e80077d4e36ed2348521
Josué Eduardo García Estrada <ge240098@alumno.udb.edu.sv>
2024-10-25 21:54:58 -0600

    Cambios del menu realizados

---
+++ Desafio_03/TurnoverTicket/Controllers/CrearConciertoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TurnoverTicket.Controllers
+{
+    public class CrearConciertoController : Controller
+    {
+        // GET: CrearConcierto
+        public ActionResult CrearConcierto()
+        {
+            return View();
+        }
+    }
+}
