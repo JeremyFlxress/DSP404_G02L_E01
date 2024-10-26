900d5316dd49e906e3e6e0e1a2d254cf3aa3ee09
unknown <LiamXG04>
2024-10-26 16:53:52 -0600

    agregando las vistas

--- Desafio_03/TurnoverTicket/Controllers/LoginController.cs
+++ Desafio_03/TurnoverTicket/Controllers/LoginController.cs
@@ -8,10 +8,27 @@ namespace TurnoverTicket.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
         public ActionResult Login()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
+
+		// Acción para la página de selección de usuario
+		public ActionResult SeleccionUsuario()
+		{
+			return View();
+		}
+
+		// Acción para el login de cliente
+		public ActionResult Cliente()
+		{
+			return View();
+		}
+
+		// Acción para el registro de cliente
+		public ActionResult RegistroCliente()
+		{
+			return View();
+		}
     }
 }
